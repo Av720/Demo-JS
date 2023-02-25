ee937df4842b08a36d38766c82cbd3f28819a6ed
Av720 <120581801+Av720@users.noreply.github.com>
2023-02-25 02:24:59 -0700

    added ajax method

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function requestBreweries() {
 
     fetch(breweryURL)
         .then(function (response) {
-            console.log(response);
+            // console.log(response);
             return response.json();
         })
         .then(function (data) {
@@ -47,3 +47,118 @@ function requestBreweries() {
             }
         });
 }
+// --------------------------------------------------------------------------------------------------
+
+// TICKET MASTER API START CODE
+
+var ticketApiKey = "AGWa5vWEgQZJJbVa9ZHcAxkl7H76w1f4";
+// var mainEvent0 = $("#event-0");
+// var maineventdate0 = $("#date-0");
+// var mainDescription0 = $("#description-0");
+// var events = json._embedded.events;
+// var item = items.first();
+var page = 0;
+// var cityId = $("#typeDestination").val();
+
+goBtn.on('click', requestEvents);
+
+function requestEvents(page) {
+    var cityId = $("#typeDestination").val();
+    $('#events-panel').show();
+    $('#attraction-panel').hide();
+
+    if (page < 0) {
+        page = 0;
+        return;
+    }
+    if (page > 0) {
+        if (page > requestEvents.json.page.totalPages - 1) {
+            page = 0;
+        }
+    }
+
+    $.ajax({
+        type: "GET",
+        url: "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=AGWa5vWEgQZJJbVa9ZHcAxkl7H76w1f4&sort=date,asc" + "&city=" + cityId + "&" + page,
+        async: true,
+        dataType: "json",
+        success: function (json) {
+            requestEvents.json = json;
+            showEvents(json);
+        },
+
+    });
+}
+
+
+function showEvents(json) {
+
+    // console.log(json)
+    var items = $('#events .list-group-item');
+    items.hide();
+    var events = json._embedded.events;
+
+    console.log(events)
+    var item = items.first();
+    for (var i = 0; i < events.length; i++) {
+        item.children('.list-group-item-heading').text(events[i].name);
+        item.children('.list-group-item-text').text(events[i].dates.start.localDate);
+
+
+    }
+}
+
+requestEvents(page);
+
+
+
+
+
+
+
+
+//old code
+// function requestEvents(page) {
+//     var cityId = $("#typeDestination").val();
+
+//     var ticketURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=AGWa5vWEgQZJJbVa9ZHcAxkl7H76w1f4&sort=date,asc" + "&city=" + cityId;
+
+//     fetch(ticketURL)
+//         .then(function (response) {
+//             // console.log(response);
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//             if (page < 0) {
+//                 page = 0;
+//                 return;
+//             }
+//             if (page > 0) {
+//                 if (page > getEvents.json.page.totalPages - 1) {
+//                     page = 0;
+//                     return;
+//                 }
+//             }
+
+
+
+
+
+//             // for (var i = 0; i < events.length; i++) {
+//             //     mainEvent0.text(events.name)
+
+
+
+
+
+
+
+
+//             // for (var i = 0; i < data.length; i++) {
+
+//             // }
+
+//             // }
+//         })
+// }
